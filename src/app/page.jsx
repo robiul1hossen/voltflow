836c22982cd2ce696658ede6aea2e6b2814f0fb4
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-03-10 17:52:42 +0600

    initialize the projects

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      this is home page
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default page;
